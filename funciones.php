0b79a95b328228b17899aa6d443762d70884f5ca
Bossun <Kazehaya.ex@gmail.com>
2016-11-05 21:40:35 -0300

    Pagina principal ahora muetra los productos más recients

--- funciones.php
+++ funciones.php
@@ -35,4 +35,37 @@
             echo "E<.";
         }
     }
+
+    function get_cProductos()
+    {
+        $contador = 1;
+        require "./config/db.php";
+        $resultado = $mysqli->query("SELECT * FROM producto ORDER BY fecha_actualizacion DESC LIMIT 4");
+        if($resultado->num_rows > 0)
+        {
+            while ($row = mysqli_fetch_object($resultado))
+            {
+                if ($contador == 1)
+                {
+                 echo "<div class=\"item active\">
+    				  <a href=><img src=\"./get_img_producto.php?id=$row->id\" alt=\"...\"></a>
+    				  <div class=\"carousel-caption\">
+    					<h3>$row->nombre<h3>
+    				  </div>
+    				</div>";
+                }
+                else
+                {
+                    echo "<div class=\"item\">
+    				  <img src=\"./get_img_producto.php?id=$row->id\"alt=\"...\">
+    				  <div class=\"carousel-caption\">
+    					<h3>$row->nombre<h3>
+    				  </div>
+    				</div>";
+                }
+                $contador += 1;
+    				
+            }    
+        }
+    }
 ?>
--- index.php
+++ index.php
@@ -108,28 +108,12 @@
     				<li data-target="#carrusel-de-nino" data-slide-to="0" class="active"></li>
     				<li data-target="#carrusel-de-nino" data-slide-to="1"></li>
     				<li data-target="#carrusel-de-nino" data-slide-to="2"></li>
+                    <li data-target="#carrusel-de-nino" data-slide-to="3"></li>
     			  </ol>
 
     			  <!-- Wrapper for slides -->
     			  <div class="carousel-inner" role="listbox">
-    				<div class="item active">
-    				  <img src="./assets/images/celular.jpg" alt="...">
-    				  <div class="carousel-caption">
-    					<h3>Celular<h3>
-    				  </div>
-    				</div>
-    				<div class="item">
-    				  <img src="./assets/images/guitarra.jpg" alt="...">
-    				  <div class="carousel-caption">
-    					<h3>Guitarras<h3>
-    				  </div>
-    				</div>
-    				<div class="item">
-    				  <img src="./assets/images/vehiculo.jpg" alt="...">
-    				  <div class="carousel-caption">
-    					<h3>Vehiculos<h3>
-    				  </div>
-    				</div>
+    				<?php get_cProductos()?>
     			  </div>
 
     			  <!-- Controls -->
